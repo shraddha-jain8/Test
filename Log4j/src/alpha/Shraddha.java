9d8dfc598e1bc07fbaa0f5296fdbf06197403bd4
Shraddha-jain8 <shraddhajn8@gmail.com>
2020-03-11 16:50:32 -0700

    first commit

---
+++ Log4j/src/alpha/Shraddha.java
@@ -0,0 +1,21 @@
+package alpha;
+
+import org.apache.logging.log4j.*;
+
+public class Shraddha {
+
+private static Logger log = LogManager.getLogger(Shraddha.class.getName());
+
+
+	public static void main(String[] args) {
+		log.info("wrong information");
+		// TODO Auto-generated method stub
+		log.error("object not present");
+		log.info("wrong info");
+		log.debug("object present");
+		log.fatal("logged");
+	
+
+	}
+
+}
---
+++ Log4j/src/beta/Ronak.java
@@ -0,0 +1,23 @@
+package beta;
+import org.apache.logging.log4j.*;
+
+
+
+public class Ronak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Logger log = LogManager.getLogger(Ronak.class.getName());
+
+
+		
+		log.info("Ronakjkdj");
+		// TODO Auto-generated method stub
+		log.error("Ronakjkdjobject not present");
+		log.info("Ronakjkdjwrong info");
+		log.debug("RonakjkdjRonakjkdjobject present");
+		log.fatal("Ronak logged");
+		
+	}
+
+}
